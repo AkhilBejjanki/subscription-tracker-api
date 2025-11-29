fe2e3e4b040cff32ec1246ebab359f2731747350
AkhilBejjanki <akhilbejjanki02@gmail.com>
2025-11-29 18:09:39 +0530

    add new controllers, arcjet

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import cookieParser from 'cookie-parser';
 import {PORT} from './config/env.js';
 
 import userRouter from './routes/user.routes.js';
-import authRoter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 
 const app = express();
@@ -16,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( { extended: false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
